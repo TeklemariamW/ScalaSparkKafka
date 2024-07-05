c52a9f74bd6698221bcce4db79b3a7622a8e230a
Teklemariam Weldehawariat <tekaliklfe@gmail.com>
2024-07-05 10:27:25 -0400

    update read from kafka

--- src/main/scala/ReadFromKafka.scala
+++ src/main/scala/ReadFromKafka.scala
@@ -1,4 +1,4 @@
-//package kafkaspark
+/*//package kafkaspark
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
@@ -44,4 +44,59 @@ object ReadFromKafka {
 
   }
 
+}*/
+//package kafkaspark
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+object ReadFromKafka {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("KafkaToJson")
+      .master("local[*]")
+      .getOrCreate()
+
+    // Define the Kafka parameters
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> "ip-172-31-3-80.eu-west-2.compute.internal:9092",
+      "key.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer",
+      "value.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer",
+      "group.id" -> "group1",
+      "auto.offset.reset" -> "earliest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
+
+    // Define the Kafka topic to subscribe to
+    val topic = "arrivaldata" // Change the topic to your new topic
+
+    // Define the schema for the JSON messages
+    val schema = StructType(Seq(
+      StructField("Bathrooms", IntegerType, nullable = true),
+      StructField("Bedrooms", IntegerType, nullable = true),
+      StructField("Neighborhood", StringType, nullable = true),
+      StructField("Price", DoubleType, nullable = true),
+      StructField("SquareFeet", IntegerType, nullable = true),
+      StructField("YearBuilt", IntegerType, nullable = true)
+    ))
+
+    // Read the JSON messages from Kafka as a DataFrame
+    val df = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "ip-172-31-3-80.eu-west-2.compute.internal:9092")
+      .option("subscribe", topic)
+      .option("startingOffsets", "earliest")
+      .load()
+      .select(from_json(col("value").cast("string"), schema).as("data"))
+      .selectExpr("data.*")
+
+    // Write the DataFrame as CSV files to HDFS (or any other storage)
+    df.writeStream
+      .format("csv")
+      .option("checkpointLocation", "/tmp/jenkins/kafka/houseprice/checkpoint")
+      .option("path", "/tmp/jenkins/kafka/houseprice/data")
+      .start()
+      .awaitTermination()
+  }
 }
